2c7e6e5fb489116baf8d1ee1a3b825888d222d83
lxq <wb-lxq302621@alibaba-inc.com>
2018-01-10 11:47:59 +0800

    合成事件

---
+++ 合成事件/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM  from "react-dom";
+
+class Demo extends React.Component {
+
+    render() {
+        return (
+            <div>
+                更
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<Demo />, document.querySelector("#root"))
